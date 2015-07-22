3f73e7f67875f84911283fbed1418656edd747be
Vinh Nguyen <nguyenducvinh92@gmail.com>
2015-07-22 12:51:34 +0700

    update static pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title page_title = ""
+    base_title = t("FELS")
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
