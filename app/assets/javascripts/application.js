393823d19a776e8bfc3491bea8f5d6aa84b88d2e
hellovietnam93 <hellovietnam93@gmail.com>
2015-07-24 10:30:51 +0700

    Update profile user

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,8 +15,18 @@
 //= require bootstrap
 //= require turbolinks
 //= require_tree .
+//= require_self
   $(document).ready(function(){
     $("div.alert").delay(3000).slideUp();
+  });
+
+  $(document).ready(function(){
+    $("#user_avatar").bind("change", function() {
+      var size_in_megabytes = this.files[0].size/1048576;
+      if (size_in_megabytes > 5) {
+        alert("Maximum file size is 5MB. Please choose a smaller file.");
+      }
+    });
   });
 
 
