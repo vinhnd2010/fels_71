b6ecc8764102bba5423655003bf8cb2beaf01d40
hellovietnam93 <hellovietnam93@gmail.com>
2015-07-26 12:48:51 +0700

    filter word

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,9 +1,19 @@
 class Word < ActiveRecord::Base
-  belongs_to :user
   belongs_to :category
 
   has_many :answers, dependent: :destroy
   has_many :lessons, through: :lesson_words
   has_many :lesson_words, dependent: :destroy
 
+  scope :in_category, ->category_id{where category_id: category_id if category_id.present?}
+  scope :learned, ->user{where("id IN (
+    SELECT word_id FROM results WHERE lesson_id IN ( SELECT id FROM lessons WHERE user_id = ?
+      ))",user.id)}
+  scope :not_learned, ->user{where("id NOT IN (
+    SELECT word_id FROM results WHERE lesson_id IN ( SELECT id FROM lessons WHERE user_id = ?
+      ))",user.id)}
+  scope :get_all, ->name{where("id IN(
+    SELECT id FROM words)")}
+  scope :alphabet, ->name{where("id IN(
+    SELECT id FROM words)").order("name")}
 end
--- db/migrate/20150722035649_create_results.rb
+++ db/migrate/20150722035649_create_results.rb
@@ -2,7 +2,7 @@ class CreateResults < ActiveRecord::Migration
   def change
     create_table :results do |t|
       t.integer :mark
-      t.references :user, index: true, foreign_key: true
+      t.references :word, index: true, foreign_key: true
       t.references :answer, index: true, foreign_key: true
       t.references :lesson, index: true, foreign_key: true
 
